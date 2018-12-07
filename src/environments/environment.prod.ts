e77758abc0ee1d70f9cf1755206b8d86699b53ca
WebAPI Service Account <misdev-api@vmsinc.org>
2018-12-07 13:57:23 -0800

    evironment reqsUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   usersUrl: 'http://GRF-MISDEV:3001/api/users',
   jobsUrl: 'http://GRF-MISDEV:3001/api/jobs',
   invsUrl: 'http://GRF-MISDEV:3001/api/invoices',
+  reqsUrl: 'http://GRF-MISDEV:3001/api/requisitions',
   authUrl: 'http://GRF-MISDEV:8888/',
   ADUrl: 'http://GRF-MISDEV:3000/api/users'
 };
